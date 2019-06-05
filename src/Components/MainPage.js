b8d8c9f3c60fca933d286d894225397fed15343a
misraelson <misraelson@gmail.com>
2019-06-04 23:01:52 -0700

    Style batman boxes

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -6,25 +6,25 @@ const term = "batman"
 const ENDPOINT = `http://api.tvmaze.com/search/shows?q=${term}`
 
 function Batman(batman, index) {
-  console.log("this is batman show", batman.show)
   return (
-    <div key={index} className={"batman" + index}>
-      <img src={batman.show.image.medium} alt={batman.show.name}/>
-      <div className="show-info">Show Name: {batman.show.name}</div>
-      <div className="show-info">Premiere Date: {batman.show.premiered}</div>
-      <div className="show-info">Rating: {batman.show.rating.average}</div>
-      <div className="show-info">Type: {batman.show.type} </div>
-      <div className="show-info">Plot Summary: <span dangerouslySetInnerHTML={{__html: batman.show.summary}}/></div>
+    <div key={index} className="batman--show">
+      <div className="show-image"><img src={batman.show.image.medium} alt={batman.show.name}/></div>
+      <div className="show-info">
+        <p>Show Name: {batman.show.name}</p>
+        <p>Premiere Date: {batman.show.premiered}</p>
+        <p>Rating: {batman.show.rating.average}</p>
+        <p>Type: {batman.show.type} </p>
+        <p>Plot Summary: <span dangerouslySetInnerHTML={{__html: batman.show.summary}}/></p>
+      </div>
     </div>
   )
 }
 
 export default function MainPage() {
-
   const [batman, setBatman] = useState([]);
   EndpointData(ENDPOINT, setBatman);
   return (
-    <div className="Batman-box">
+    <div className="Batman--box">
       {batman.map( (batman, index) => (Batman(batman, index)))}
     </div>
   )
